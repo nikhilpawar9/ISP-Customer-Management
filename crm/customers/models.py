387afa2fd27badd7e98f8a95e38caff2c9cc1d1a
Nikhil <nikhilpawar982001@gmail.com>
2021-05-15 13:30:51 +0530

    Sent Customer info to database

--- crm/customers/models.py
+++ crm/customers/models.py
@@ -1,30 +1,35 @@
 from django.db import models
 
 
-PLAN = (
-    ('plan1','plan1'),
-    ('plan2','plan2'),
-    ('plan3','plan3'),
-)
-CONNECTION_TYPE = (
-    ('connectiontype1','connectiontype1'),
-    ('connectiontype2','connectiontype2'),
-    ('connectiontype3','connectiontype3'),
-)
-ROUTER_MODEL = (
-    ('router1','router1'),
-    ('router2','router2'),
-    ('router3','router3'),
-)
-# Create your models here.
 class Customer(models.Model):
     name = models.CharField(max_length=100, primary_key = True)
-    address = models.CharField(max_length=400)
-    connection_type = models.CharField(max_length=20, choices=CONNECTION_TYPE)
-    router = models.CharField(max_length=20, choices=ROUTER_MODEL)
-    plan = models.CharField(max_length=20, choices=PLAN)
-    primary_mobile = models.IntegerField(max_length=10)
-    alternate_mobile = models.IntegerField(max_length=10, null=True)
-    #image= models.ImageField(upload_to="customers/images",default="")
+    email = models.CharField(max_length=100, default= "admin@ithreesystem.com")
+    primary_mobile = models.IntegerField( default=0)
+    alternate_mobile = models.IntegerField( default=0 )
+    address = models.CharField(max_length=400, default="Jejuri")
+    city = models.CharField(max_length=10, default="Jejuri")
+    state = models.CharField(max_length=15, default="Maharashtra")
+    zipcode = models.IntegerField (default=0, blank=True)
+    
+    connection_type = models.CharField( max_length=20 ,default="not selected")
+    
+    # wrieless
+    cat_6_cable_length= models.IntegerField( )
+    p2p_device_price= models.IntegerField( )
+    wireless_router_price= models.IntegerField( )
+    
+    # ftth
+    ftth_fiber_length= models.IntegerField( )
+    closer_box = models.CharField(max_length=5 )
+    patch_cord= models.IntegerField( )
+    ftth_router_price= models.IntegerField()
+
+    
+    installation_charges= models.IntegerField( default=0)
+    username= models.CharField(max_length=10, default="username")
+    password= models.CharField(max_length=10, default="password")
+    
+    customer_photo= models.ImageField(upload_to="customers/images",default="", blank=True)
+    kYC_document= models.ImageField(upload_to="customers/images",default="", blank=True)
     def __str__(self):
         return self.name
--- crm/customers/views.py
+++ crm/customers/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from customers.models import Customer
 
-# Create your views here.
 def home(request):
     return render (request, 'customers/dashboard.html')
 
@@ -10,4 +10,44 @@ def customers(request):
 
 
 def addcustomer(request):
+    if (request.method== 'POST'  ) :
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        primarymobile = request.POST.get('primarymobile')
+        alternatemobile = request.POST.get('alternatemobile')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zipcode = request.POST.get('zipcode')
+        connectiontype = request.POST.get('connectiontype')
+        
+        if connectiontype == 'wireless':
+            catcablelength= request.POST.get('catcablelength')
+            p2pdevice= request.POST.get('p2pdevice')
+            wirelessrouterprice= request.POST.get('wirelessrouterprice')
+            ftthfiberlength = '0'
+            ftthrouterprice = '0'
+            closerbox= '0'
+            patchcord= '0'
+        
+        elif connectiontype == 'ftth':
+            catcablelength= '0'
+            p2pdevice= '0'
+            wirelessrouterprice= 0            
+            ftthfiberlength = request.POST.get('ftthfiberlength')
+            ftthrouterprice = request.POST.get('ftthrouterprice')
+            closerbox= request.POST.get('closerbox')
+            patchcord= request.POST.get('patchcord')
+        
+        installationcharges= request.POST.get('installationcharges')
+        ontusername= request.POST.get('ontusername')
+        ontpassword= request.POST.get('ontpassword')
+        customerphoto= request.POST.get('customerphoto')
+        kycdocument= request.POST.get('kycdocument')
+        
+        addcustomer = Customer(name = name, email=email, primary_mobile= primarymobile, alternate_mobile = alternatemobile, address=address, city=city, state=state, zipcode=zipcode, connection_type= connectiontype, cat_6_cable_length = catcablelength, p2p_device_price=p2pdevice, wireless_router_price=wirelessrouterprice, ftth_fiber_length = ftthfiberlength, closer_box = closerbox, patch_cord = patchcord, ftth_router_price = ftthfiberlength, installation_charges= installationcharges, username= ontusername, password = ontpassword, customer_photo = customerphoto, kYC_document = kycdocument)
+        addcustomer.save()
     return render (request, 'customers/addcustomer.html')
+
+
+
